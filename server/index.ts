f4be625850b6da5b4afb7c5b4e471a26c3d95eb0
MrLexsmyth <shittuodunayo4@gmail.com>
2025-07-02 17:46:29 +0100

    hey

--- server/index.ts
+++ server/index.ts
@@ -1,13 +1,13 @@
 // server/index.ts
 import express from 'express';
 import cors from 'cors';
-import userRoutes from './routes/userRoutes';
+
 
 const app = express();
 app.use(cors());
 app.use(express.json());
 
-app.use('/api/users', userRoutes);
+app.use('/api/users', );
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
